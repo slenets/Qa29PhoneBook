80b1b3dd46cceb84080bad5dd0be57a246437bd0
Slava <slavalenets.dev@gmail.com>
2022-01-10 08:45:37 +0200

    Add driver initialization method
    New browser initialized to Web driver

--- src/main/java/application/ApplicationManager.java
+++ src/main/java/application/ApplicationManager.java
@@ -2,6 +2,9 @@ package application;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,9 +12,24 @@ public class ApplicationManager {
     WebDriver wd;
     UserHelper userHelper;
     ContactHelper contactHelper;
+    String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init(){
-        wd = new ChromeDriver();
+        if(browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        }else if(browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        }else if (browser.equals(BrowserType.EDGE)){
+            wd = new EdgeDriver();
+        }
+
+
+
+
         wd.manage().window().maximize();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
